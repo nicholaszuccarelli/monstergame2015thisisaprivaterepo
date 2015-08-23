80703034a4abf5b7275b09b1da25774ab6145b00
rmit-s3546815-nicholas-zuccarelli <s3546815@student.rmit.edu.au>
2015-08-23 15:56:46 +1000

    Gameboard.java initial commit

---
+++ SERVERSIDE/src/gameboard/Gameboard.java
@@ -0,0 +1,40 @@
+package gameboard;
+
+import java.util.UUID;
+
+public class Gameboard {
+	private String ID;
+	private char design[][];
+	
+	public Gameboard()
+	{
+		this.ID = UUID.randomUUID().toString();
+		this.design = new char[][] {
+			{'x','x','x','x','x'},
+			{'x','y','x','y','x'},
+			{'x','x','x','x','x'},
+			{'x','y','x','y','x'},
+			{'x','x','x','x','x'}
+		};
+	}
+	
+	public String getID()
+	{
+		return ID;
+	}
+	
+	public char[][] getDesign()
+	{
+		return design;
+	}
+	
+	public boolean updateGameboard(int x, int y, Player p)
+	{
+		if(getDesign()[x][y] != 'y')
+		{
+			design[x][y] = p.getBoardID();
+			return true;
+		}
+		return false;
+	}
+}
